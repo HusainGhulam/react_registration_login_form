238c4da1e5784ebb82b40b592f075d011449cf50
HusainGhulam <husainghulam993@gmail.com>
2024-12-30 13:54:09 +0530

    Registration and login form with react 19

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Registration and login form
+
+By Default registration fomr enable in app.jsx file
+If you want enable login form you can uncomment "LoginFormReact" and LoginFormReact from app.jsx
+We can check all value on consolve after submit register form or login form
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { LoginFormReact } from "./components/Hooks/useState/LoginReact";
+
+import { RegistrationFormReact } from "./components/Hooks/useState/RegistrationFormReact";
+export const App = () => {
+  return (
+    <>
+      <section>
+        {/* <LoginFormReact /> */}
+        <RegistrationFormReact />
+      </section>
+    </>
+  );
+};
